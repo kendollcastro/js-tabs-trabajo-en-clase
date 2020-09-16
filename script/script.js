e704f4d6f0974841f12482ae27b60e9597316af1
kendollcastro <kendollcastro@gmail.com>
2020-09-15 19:45:58 -0600

    Agregamos el codigo al github

--- script/script.js
+++ script/script.js
@@ -1,37 +1,35 @@
-function clickhandler(event){
-    const elemento = event.currentTarget; 
-    const href = elemento.getAttribute('href');
-    const id = href.substring(1);
-    console.log(id);
 
-    //****Falto la parte de ocultar, estaba analizandola porque si me cuesta un poco Profe****
-    for (let i = 0; i < elemento.length; i++){
-        elemento[i].style.display = 'none';
-        if (elemento[i].getAttribute('id') == id){
-            elemento[i].style.display = 'block';
-        }
-    }
-}   
-
-
+//Para ocultar los elementos.
+for(let i = 0; i < contenido.length; i++){
+    contenido[i].classList.add('js-content-hidden');
+}
 //Vamos a seleccionar todos los items en los cuales vamos a trabajar, para las tab de las tabla
 
 const enlaces = document.querySelectorAll('.tabs-tablist-item');
 
-// Vamos a agregar todos los tab content para 
+//Para obtener los divs del contenido
 
 const contenido = document.querySelectorAll('.tabs-content-item');
 
 //Vamos a seleccionar los elementos de la lista, para que no se muestren otros que no queremos 
 
-for(let i = 0; i < contenido.length; i++){
-    contenido[i].style.display = 'none';
-}
+for(let i = 0; i < enlaces.length; i++){
+    enlaces[i].addEventListener('click', (event) => {
+        console.log(event);
+        event.preventDefault();
+        const id = event.currentTarget.getAttribute('href').substring(1);
+        console.log(id);
 
- contenido[0].style.display = 'block';
-
-//Vamos a hacer el for para la eleccion de los 
-
-for (let i = 0; i < enlaces.length; i++){
-    enlaces[i].addEventListener('click', clickhandler)
+        for(let i = 0; i < contenido.length; i++){
+            contenido[i].classList.add('js-content-hidden');
+            if(contenido[i].getAttribute('id') == id){
+                contenido[i].classList.remove('js-content-hidden');
+            }
+        }
+    
+        for (let i = 0; i < enlaces.length; i++){
+            enlaces[i].classList.remove('js-content-hidden');
+        }
+        event.currentTarget.classList.add('js-active');
+    });
 }
